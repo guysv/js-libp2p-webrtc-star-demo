d948e6c29eb32dd4cf6b120b6cf951c6118b4e44
guysv <sviryguy@gmail.com>
2019-05-17 16:46:20 +0300

    Make the direct connection more obvious

--- src/dialer.js
+++ src/dialer.js
@@ -9,12 +9,13 @@ const PeerId = require('peer-id')
 const PeerInfo = require('peer-info')
 const Node = require('./bundle')
 const pull = require('pull-stream')
-const series = require('async/series')
+const waterfall = require('async/waterfall')
 
 let listenerId
 let listenerNode
+let conn
 
-series([
+waterfall([
   (cb) => {
     PeerId.createFromJSON(require('./id-d'), (err, id) => {
       if (err) { return cb(err) }
@@ -47,23 +48,32 @@ series([
     listenerNode.dialProtocol(`/p2p-webrtc-star/ipfs/${require('./id-l').id}`, '/echo/1.0.0', cb)
     // console.log(`/p2p-circuit/ipfs/${require('./id-l').id}`)
     // cb()
-  }
-], (err, conn) => {
-  if (err) { throw err }
-
-  console.log('Listening on:')
-  listenerNode.peerInfo.multiaddrs.forEach((ma) => {
-    console.log(ma.toString() + '/p2p/' + listenerId.toB58String())
-  })
-
-  console.log(`Sending data to /p2p-webrtc-star/ipfs/${require('./id-l').id}`)
-
-  pull(
-    pull.values(['hey']),
-    conn.conn, // TODO: why conn.conn???
-    pull.collect((err, data) => {
-      if (err) { throw err }
-      console.log('received echo:', data.toString())
+  },
+  (newConn, cb) => {
+    conn = newConn
+    console.log(`Hanging up /ip4/0.0.0.0/tcp/10333/ipfs/${require('./id-r').id}`)
+    listenerNode.hangUp(`/ip4/0.0.0.0/tcp/10333/ipfs/${require('./id-r').id}`, cb)
+  },
+  (cb) => {
+    console.log('Current connections:')
+    Object.values(listenerNode.peerBook.getAll()).forEach((peer) => {
+      const addr = peer.isConnected()
+      if (addr) {
+        console.log(addr.toString())
+      }
     })
-  )
+  
+    console.log(`Sending data to /p2p-webrtc-star/ipfs/${require('./id-l').id}`)
+  
+    pull(
+      pull.values(['hey']),
+      conn.conn, // TODO: why conn.conn???
+      pull.collect((err, data) => {
+        if (err) { throw err }
+        console.log('received echo:', data.toString())
+      })
+    )
+  }
+], (err) => {
+  if (err) { throw err }
 })
